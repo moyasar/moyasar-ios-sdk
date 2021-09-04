cc809d9d6da4936d8bef828b481899f86b410934
Ali Alhoshaiyan <alialhoshaiyan@gmail.com>
2021-09-04 17:36:41 +0300

    Initial Commit

---
+++ MoyasarSdk/MoyasarSdk.h
@@ -0,0 +1,18 @@
+//
+//  MoyasarSdk.h
+//  MoyasarSdk
+//
+//  Created by Ali Alhoshaiyan on 04/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MoyasarSdk.
+FOUNDATION_EXPORT double MoyasarSdkVersionNumber;
+
+//! Project version string for MoyasarSdk.
+FOUNDATION_EXPORT const unsigned char MoyasarSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoyasarSdk/PublicHeader.h>
+
+
