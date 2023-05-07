8a4ec7968428a74c28540dc075c0f1206e7cd6c5
Ali <ahoshaiyan@fastmail.com>
2023-05-07 12:39:00 +0300

    Fix expiry validation and translation (#7)
    * Fix expiry validation and translation
    
    * mend

--- Sdk/MoyasarSdk/Helpers/ExpiryDate.swift
+++ Sdk/MoyasarSdk/Helpers/ExpiryDate.swift
@@ -10,33 +10,35 @@ import Foundation
 struct ExpiryDate {
     var month: Int
     var year: Int
-    
+
     static var cal = Calendar(identifier: .gregorian)
-    
+
     func isValid() -> Bool {
         return month >= 1 && month <= 12 && year > 1900
     }
-    
+
     func expired() -> Bool {
         guard let expiry = dateInstance() else {
             return false
         }
-        return expiry < Date()
+
+        let date = ExpiryDate.cal.date(from: ExpiryDate.cal.dateComponents([.year, .month], from: Date()))
+        return expiry < date!
     }
-    
+
     func inexpired() -> Bool {
         return !expired()
     }
-    
+
     private func dateInstance() -> Date? {
         if !isValid() {
             return nil
         }
-        
+
         var components = DateComponents()
         components.month = month
         components.year = year
-        
+
         return ExpiryDate.cal.date(from: components)
     }
 }
@@ -46,11 +48,11 @@ extension ExpiryDate {
         let clean = pattern
             .replacingOccurrences(of: " ", with: "")
             .replacingOccurrences(of: "/", with: "")
-        
+
         if clean.count == 4 {
             let millennium = (ExpiryDate.cal.dateComponents([.year], from: Date()).year! / 100) * 100
             let year = Int(clean.substringByInt(start: 2, length: 2)) ?? 0
-            
+
             return ExpiryDate(
                 month: Int(clean.substringByInt(start: 0, length: 2)) ?? 0,
                 year: year + millennium)
