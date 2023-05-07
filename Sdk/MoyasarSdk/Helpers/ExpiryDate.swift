a3aaa3501e7d634a0e3254b73745992d0f30ac55
Ali Alhoshaiyan <ahoshaiyan@fastmail.com>
2023-05-07 12:38:30 +0300

    mend

--- Sdk/MoyasarSdk/Helpers/ExpiryDate.swift
+++ Sdk/MoyasarSdk/Helpers/ExpiryDate.swift
@@ -10,35 +10,35 @@ import Foundation
 struct ExpiryDate {
     var month: Int
     var year: Int
-    
+
     static var cal = Calendar(identifier: .gregorian)
-    
+
     func isValid() -> Bool {
         return month >= 1 && month <= 12 && year > 1900
     }
-    
+
     func expired() -> Bool {
         guard let expiry = dateInstance() else {
             return false
         }
-        
+
         let date = ExpiryDate.cal.date(from: ExpiryDate.cal.dateComponents([.year, .month], from: Date()))
         return expiry < date!
     }
-    
+
     func inexpired() -> Bool {
         return !expired()
     }
-    
+
     private func dateInstance() -> Date? {
         if !isValid() {
             return nil
         }
-        
+
         var components = DateComponents()
         components.month = month
         components.year = year
-        
+
         return ExpiryDate.cal.date(from: components)
     }
 }
@@ -48,11 +48,11 @@ extension ExpiryDate {
         let clean = pattern
             .replacingOccurrences(of: " ", with: "")
             .replacingOccurrences(of: "/", with: "")
-        
+
         if clean.count == 4 {
             let millennium = (ExpiryDate.cal.dateComponents([.year], from: Date()).year! / 100) * 100
             let year = Int(clean.substringByInt(start: 2, length: 2)) ?? 0
-            
+
             return ExpiryDate(
                 month: Int(clean.substringByInt(start: 0, length: 2)) ?? 0,
                 year: year + millennium)
@@ -63,15 +63,5 @@ extension ExpiryDate {
         } else {
             return nil
         }
-    }
-}
-
-extension Date {
-    func startOfMonth() -> Date {
-        return Calendar.current.date(from: Calendar.current.dateComponents([.year, .month], from: Calendar.current.startOfDay(for: self)))!
-    }
-    
-    func endOfMonth() -> Date {
-        return Calendar.current.date(byAdding: DateComponents(month: 1, day: -1), to: self.startOfMonth())!
     }
 }
