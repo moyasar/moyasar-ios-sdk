bc38902ef7c657c89373236dddd34416823cc869
Mahmoud-Abdelwahab <mahmoud.elziany@gmail.com>
2024-06-30 13:03:17 +0300

    Feature/swift package manager support (#40)
    * adding package.swift file to the sdk
    
    * update package.swift file location
    
    * update sdk path
    
    * adding  package.swift
    
    * Exclude unwanted root-level directories
    
    * update package.swift
    
    * update package.swift
    
    * update
    
    * update
    
    * update package.swift
    
    * update package.swift
    
    * update package.swift
    
    * update package.swift
    
    * update readme file
    
    * update readme file
    
    ---------
    
    Co-authored-by: Mahmoud abdelwahab <mahmoud.abdelwahab@xische.com>

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.10
+import PackageDescription
+
+let package = Package(
+    name: "MoyasarSdk",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v13),
+    ],
+    products: [
+        .library(
+            name: "MoyasarSdk",
+            targets: ["MoyasarSdk"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "MoyasarSdk",
+            path: "Sdk/MoyasarSdk",
+            exclude: ["Info.plist"],
+            resources: [
+                .process("Media.xcassets"),
+                .process("ar.lproj"),
+                .process("en.lproj"),
+                .process("PrivacyInfo.xcprivacy")
+            ]
+        ),
+        .testTarget(
+            name: "MoyasarSdkTests",
+            dependencies: ["MoyasarSdk"],
+            path: "Sdk/MoyasarSdkTests",
+            exclude: ["Info.plist"]
+        ),
+    ]
+)
