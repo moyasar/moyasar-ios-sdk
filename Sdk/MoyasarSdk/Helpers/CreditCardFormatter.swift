7398cd0a7b0e152c3a047ae9a3184fc817d1a5df
Mahmoud abdelwahab <mahmoud.abdelwahab@xische.com>
2024-07-11 07:35:14 +0300

    fixing the formatter issues and use the new CreditCardTextField with uikit

--- Sdk/MoyasarSdk/Helpers/CreditCardFormatter.swift
+++ Sdk/MoyasarSdk/Helpers/CreditCardFormatter.swift
@@ -8,51 +8,69 @@
 import Foundation
 import SwiftUI
 
-final class CreditCardFormatter: Formatter {
-    override func string(for obj: Any?) -> String? {
-        if let string = obj as? String {
-            return formatCardNumber(number: string)
-        }
-        return nil
-    }
+final class CreditCardFormatter {
     
-    override func getObjectValue(_ obj: AutoreleasingUnsafeMutablePointer<AnyObject?>?, for string: String, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
-        obj?.pointee = cleanNumber(string) as AnyObject
-        return true
-    }
-    
-    private func formatCardNumber(number: String) -> String {
-        if number.hasPrefix("34") {
-            return formatAmexNumber(number)
-        }
+    /// Formats the credit card number based on the card type
+    func formatCardNumber(_ number: String) -> String {
+        let cleaned = cleanNumber(number)
         
-        return formatVisaNumber(number)
-    }
-    
-    private func formatAmexNumber(_ number: String) -> String {
-        // 4-6-5
-        return insertSpacesAtPosition(number, positions: [4, 10], max: 16)
-    }
-    
-    private func formatVisaNumber(_ number: String) -> String {
-        // 4-4-4-4
-        return insertSpacesAtPosition(number, positions: [4, 8, 12], max: 16)
-    }
-    
-    private func insertSpacesAtPosition(_ number: String, positions: [Int], max: Int) -> String {
-        var number = cleanNumber(number)
-
-        if number.count > max {
-            number = String(number.prefix(max))
-        }
+        // Check if the card is AMEX based on the starting digits
+        let isAMEX = cleaned.starts(with: "34") || cleaned.starts(with: "37")
         
-        for pos in positions.sorted().reversed() {
-            if number.count > pos {
-                number.insert(" ", at: number.index(number.startIndex, offsetBy: pos))
+        return isAMEX ? formatAMEXCardNumber(cleaned) : formatOtherCardNumber(cleaned)
+    }
+    
+    /// Format for AMEX: xxxx xxxxxx xxxxx  -->  4-6-5
+    ///
+    private func formatAMEXCardNumber(_ number: String) -> String {
+        var formattedNumber = ""
+        let segments = [4, 6, 5]
+        var startIndex = number.startIndex
+        
+        for segment in segments {
+            let endIndex = number.index(startIndex, offsetBy: segment, limitedBy: number.endIndex) ?? number.endIndex
+            let segmentString = String(number[startIndex..<endIndex])
+            formattedNumber += segmentString
+            if endIndex < number.endIndex {
+                formattedNumber += " "
             }
+            startIndex = endIndex
         }
         
-        return number
+        return formattedNumber
+    }
+    
+    /// Format for other cards: xxxx xxxx xxxx xxxx --> 4-4-4-4
+    ///
+    private func formatOtherCardNumber(_ number: String) -> String {
+        let maxLength = 16
+        let truncated = number.prefix(maxLength)
+        
+        return stride(from: 0, to: truncated.count, by: 4).map {
+            let start = truncated.index(truncated.startIndex, offsetBy: $0)
+            let end = truncated.index(start, offsetBy: 4, limitedBy: truncated.endIndex) ?? truncated.endIndex
+            return String(truncated[start..<end])
+        }.joined(separator: " ")
+    }
+    
+    /// Formats the expiry date (e.g., "1225" to "12/25")
+    ///
+    func formatExpiryDate(_ date: String) -> String {
+        let cleaned = cleanNumber(date)
+        if cleaned.count > 2 {
+            let month = cleaned.prefix(2)
+            let year = cleaned.suffix(from: cleaned.index(cleaned.startIndex, offsetBy: 2))
+            return "\(month)/\(year)"
+        }
+        return cleaned
+    }
+    
+    /// Formats the CVC
+    ///
+    func formatCVC(_ cvc: String, forCardNumber cardNumber: String) -> String {
+        let cleaned = cleanNumber(cvc)
+        let isAMEX = cardNumber.starts(with: "34") || cardNumber.starts(with: "37")
+        return String(cleaned.prefix(isAMEX ? 4 : 3))
     }
     
     private func cleanNumber(_ number: String) -> String {
--- Sdk/MoyasarSdk/Views/Components/CreditCardInfoComponent/CreditCardInfoView+View.swift
+++ Sdk/MoyasarSdk/Views/Components/CreditCardInfoComponent/CreditCardInfoView+View.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Combine
 
 extension CreditCardInfoView {
     // MARK: - Subviews
@@ -28,13 +29,11 @@ extension CreditCardInfoView {
     var cardNumberField: some View {
         VStack(alignment: .leading) {
             ZStack(alignment: .trailing) {
-                creditCardTextField(
-                    title: "card-number".localized(),
-                    text: $cardInfo.number,
-                    keyboardType: .numberPad,
-                    autocapitalization: .none,
-                    disableAutocorrection: true
-                )
+                CreditCardTextField(
+                                   text: $cardInfo.number,
+                                   placeholder: "card-number".localized(),
+                                   formatter: cardInfo.formatter.formatCardNumber(_:)
+                ).frame(height: 46)
                 cardNetworkLogos
             }
             validationText(for: cardInfo.numberValidator.visualValidate(value: cardInfo.number))
@@ -44,13 +43,11 @@ extension CreditCardInfoView {
     /// View for the "Expiry Date" input field and validation.
     var expiryDateField: some View {
         VStack(alignment: .leading) {
-            creditCardTextField(
-                title: "expiry".localized(),
-                text: $cardInfo.expiryDate,
-                keyboardType: .numberPad,
-                autocapitalization: .none,
-                disableAutocorrection: true
-            )
+            CreditCardTextField(
+                           text: $cardInfo.expiryDate,
+                           placeholder: "expiry".localized(),
+                           formatter: cardInfo.formatter.formatExpiryDate
+                       ).frame(height: 46)
             validationText(for: cardInfo.expiryValidator.visualValidate(value: cardInfo.expiryDate))
         }
     }
@@ -58,13 +55,14 @@ extension CreditCardInfoView {
     /// View for the "CVC Code" input field and validation.
     var cvcField: some View {
         VStack(alignment: .leading) {
-            creditCardTextField(
-                title: "cvc".localized(),
-                text: $cardInfo.securityCode,
-                keyboardType: .numberPad,
-                autocapitalization: .none,
-                disableAutocorrection: true
-            )
+            CreditCardTextField(
+                           text: Binding(
+                               get: { cardInfo.securityCode },
+                               set: { cardInfo.securityCode = cardInfo.formatter.formatCVC($0, forCardNumber: cardInfo.number) }
+                           ),
+                           placeholder: "cvc".localized(),
+                           formatter: { $0 }
+                       ).frame(height: 46)
             validationText(for: cardInfo.securityCodeValidator.visualValidate(value: cardInfo.securityCode))
         }
     }
@@ -72,30 +70,38 @@ extension CreditCardInfoView {
     // MARK: - Reusable Views
     
     /// Creates a reusable text field for credit card information.
-    /// - Parameters:
-    ///   - title: The placeholder title for the text field.
-    ///   - text: A binding to the text field's value.
-    ///   - keyboardType: The keyboard type for the text field.
-    ///   - autocapitalization: The text field's autocapitalization setting.
-    ///   - disableAutocorrection: A Boolean value that indicates whether to disable autocorrection.
-    /// - Returns: A configured `TextField`.
+      /// - Parameters:
+      ///   - title: The placeholder title for the text field.
+      ///   - text: A binding to the text field's value.
+      ///   - keyboardType: The keyboard type for the text field.
+      ///   - autocapitalization: The text field's autocapitalization setting.
+      ///   - disableAutocorrection: A Boolean value that indicates whether to disable autocorrection.
+      /// - Returns: A configured `TextField`.
     private func creditCardTextField(
         title: String,
         text: Binding<String>,
         keyboardType: UIKeyboardType,
         autocapitalization: UITextAutocapitalizationType,
-        disableAutocorrection: Bool
+        disableAutocorrection: Bool,
+        formatter: @escaping (String) -> String = { $0 } // Default formatter does nothing
     ) -> some View {
-        TextField(title, text: text)
-            .keyboardType(keyboardType)
-            .autocapitalization(autocapitalization)
-            .disableAutocorrection(disableAutocorrection)
-            .padding(10)
-            .overlay(
-                RoundedRectangle(cornerRadius: 7)
-                    .stroke(Color(red: 0.7, green: 0.7, blue: 0.7), lineWidth: 0.4)
-            )
+        TextField(title, text: Binding(
+            get: { text.wrappedValue },
+            set: { newValue in
+                text.wrappedValue = formatter(newValue)
+            }
+        ))
+        .keyboardType(keyboardType)
+        .autocapitalization(autocapitalization)
+        .disableAutocorrection(disableAutocorrection)
+        .padding(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 7)
+                .stroke(Color(red: 0.7, green: 0.7, blue: 0.7), lineWidth: 0.4)
+        )
     }
+
+      
     
     /// Creates a view that displays validation error text.
     /// - Parameter validationResult: The validation result to display.
@@ -135,3 +141,4 @@ extension CreditCardInfoView {
         .padding(.trailing, 7)
     }
 }
+
